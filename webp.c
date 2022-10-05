8f907d477f2bc48bd6580e6ce4c2547036be18b5
Leonardo Hernández Hernández <leohdz172@protonmail.com>
2022-10-04 22:21:31 -0500

    webp: simplify error path

--- webp.c
+++ webp.c
@@ -15,6 +15,7 @@ webp_load(FILE *fp, const char *path)
     uint8_t *file_data = NULL;
     uint8_t *image_data = NULL;
     size_t image_size;
+    bool ok = false;
     pixman_image_t *pix = NULL;
     pixman_format_code_t format;
     int width, height, stride;
@@ -30,44 +31,42 @@ webp_load(FILE *fp, const char *path)
     }
 
     if (!(file_data = WebPMalloc(image_size + 1))) {
-        goto err;
+        goto out;
     }
 
     clearerr(fp);
     fread(file_data, image_size, 1, fp);
     if (ferror(fp)) {
         LOG_ERRNO("%s: failed to read", path);
-        goto err;
+        goto out;
     }
     file_data[image_size] = '\0';
 
     /* Verify it is a webp image */
     if (!WebPGetInfo(file_data, image_size, NULL, NULL)) {
         LOG_ERR("%s: not a WebP file", path);
-        goto err;
+        goto out;
     }
 
     image_data = WebPDecodeRGBA(file_data, image_size, &width, &height);
     if (image_data == NULL) {
-        goto err;
+        goto out;
     }
     format = PIXMAN_x8b8g8r8;
     stride = stride_for_format_and_width(format, width);
 
-    pix = pixman_image_create_bits_no_clear(
-        format, width, height, (uint32_t *)image_data, stride);
+    ok = NULL != (pix = pixman_image_create_bits_no_clear(
+        format, width, height, (uint32_t *)image_data, stride));
 
-    if (pix == NULL) {
+    if (!ok) {
         LOG_ERR("%s: failed to instantiate pixman image", path);
-        goto err;
+        goto out;
     }
 
+out:
     WebPFree(file_data);
+    if (!ok)
+        WebPFree(image_data);
+
     return pix;
-
-err:
-    WebPFree(file_data);
-    WebPFree(image_data);
-
-    return NULL;
 }
